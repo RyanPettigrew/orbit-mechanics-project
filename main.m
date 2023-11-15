20657b68ce3f6c82bfb445a763bd43daa7e56874
RyanPettigrew <ryan.pettigrew77@gmail.com>
2023-11-15 15:15:42 -0800

    Example
    updated example on how to call from the structure

--- main.m
+++ main.m
@@ -1,7 +1,14 @@
-% MAIN SCRIPT FOR FOR CALLING FUNCTIONS/ORBIT CALULATIONS
+
+%% EXAMPLE: SCRIPT FOR CALLING FUNCTIONS/ORBIT CALULATIONS
 clear; clc; close all;
 
 run orbit_data.m
-x = DATA.OBJECT1.ECCENTRICITY;
 
-disp([DATA.OBJECT1])
+% These both do the same thing
+disp(OBJECT1); 
+disp(DATA.OBJECT1);
+
+% How to get the data out of the structure; these both do the same thing
+disp(OBJECT1.ECCENTRICITY); 
+disp(DATA.OBJECT1.ECCENTRICITY);
+
--- orbit_data.m
+++ orbit_data.m
@@ -3,9 +3,16 @@ THIS FUNCTION LOADS THE .JSON ORBIT DATA STRUCTURE
 EXAMPLE ON HOW TO USE IT IN ANY SCRIPT:
 run orbit_data.m 
 x = DATA.OBJECT1.ECCENTRICITY;
+OR
+run orbit_data.m 
+x = OBJECT1.ECCENTRICITY;
 %}
 
 clear; clc; close all;
 myJsonFile = "orbits.json";
 text = fileread(myJsonFile);
 DATA = jsondecode(text);
+OBJECT1 = DATA.OBJECT1;
+OBJECT2 = DATA.OBJECT2;
+OBJECT3 = DATA.OBJECT3;
+OBJECT4 = DATA.OBJECT4;
